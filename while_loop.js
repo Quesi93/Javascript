f63fa1439f0d7a6f97d8bdb3124b42e611a25b2a
Quesi <44482817+Quesi93@users.noreply.github.com>
2022-03-30 15:58:52 +0000

    While Loop

---
+++ while_loop.js
@@ -0,0 +1,15 @@
+// While Loops
+var names = [
+    "Victor",
+    "Godspower", 
+    "Quesi", 
+    "Favor",
+    "Bright"
+];
+
+var index = 0;
+
+while(index < names.length) {
+    console.log(names[index]);
+    index = index + 1;
+}
