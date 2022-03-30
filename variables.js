1b308fbdd11b4047f0639cf2583daae951e7d51d
Quesi <44482817+Quesi93@users.noreply.github.com>
2022-03-30 14:07:28 +0000

    update on variables

--- variables.js
+++ variables.js
@@ -1,6 +1,16 @@
 var firstName = "Quesi";
 var age = 25;
 var isFemale = true;
+var balance = 500.32;
+var dob = new Date(1996, 9, 42);
+var person = {};
+var empty = undefined;
+
 
 console.log(age);
 console.log(firstName);
+console.log(isFemale);
+console.log(balance);
+console.log(dob);
+console.log(person);
+console.log(empty);
